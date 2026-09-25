4bdfe3970d9aeb2f02e12cb5d374e4cde0361737
wirecoreltd <wirecoreltd@gmail.com>
2026-09-25 11:02:24 +0400

    Delete app/layout.js

--- app/layout.js
+++
@@ -1,5 +0,0 @@
-import './globals.css'
-export const metadata = { title: 'CRM Pompes à chaleur' }
-export default function Layout({ children }) {
-  return <html lang="fr"><body>{children}</body></html>
-}
